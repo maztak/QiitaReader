af7f3ecc9debad567e95e26102a0d04ccc43cc65
Takuya Matsuda <takuya108817@gmail.com>
2018-03-09 13:25:49 +0900

    ストーリーボードで、ボタンによる画面遷移を実装

---
+++ QiitaReader/QiitaReader/KensakuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  KensakuViewController.swift
+//  QiitaReader
+//
+//  Created by Takuya Matsuda on 2018/03/09.
+//  Copyright © 2018年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class KensakuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ QiitaReader/QiitaReader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QiitaReader
+//
+//  Created by Takuya Matsuda on 2018/03/09.
+//  Copyright © 2018年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Qiitaサーバーに対してHTTP通信のリクエストを出してデータを取得
+  //      let requestUrl = "https://qiita.com/api/v2/items"
+        //新着記事データをテーブルビューに表示
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
