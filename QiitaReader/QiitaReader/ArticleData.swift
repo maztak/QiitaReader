313a204e005efdeddcb97d65a882f88b09091626
Takuya Matsuda <takuya108817@gmail.com>
2018-03-09 19:22:12 +0900

    構造体を定義してい、それによりオブジェクト（article）を作成、 ViewControllerのクラス定義などでエラーの状態

---
+++ QiitaReader/QiitaReader/ArticleData.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleData.swift
+//  QiitaReader
+//
+//  Created by Takuya Matsuda on 2018/03/09.
+//  Copyright © 2018年 mycompany. All rights reserved.
+//
+
+import Foundation
+
+//structをオブジェクトに表現する書き方？＝イニシャライザ？
+var article = Article1 (
+    title: "初心者がオリジナルのiOSアプリを開発できるようになるまで",
+    authorName: "justin999",
+    authorIcon: ".../images/1473687346.jpeg",
+    tag1: "iOS",
+    tag2: "swift",
+    tag3: "Objective-C",
+    goodCountLabel: "142",
+    articleText: "私はiOSアプリのフロント側を趣味や仕事で開発しています。そんな中、時折聞かれるのが「iOSアプリってどうやって開発するの？"
+)
--- QiitaReader/QiitaReader/ViewController.swift
+++ QiitaReader/QiitaReader/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    //テーブルビュー
+    @IBOutlet var table :UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Qiitaサーバーに対してHTTP通信のリクエストを出してデータを取得
-  //      let requestUrl = "https://qiita.com/api/v2/items"
-        //新着記事データをテーブルビューに表示
-    }
-    
-    override func viewWillAppear() {
+        //Table View　のDataSouce参照先指定
+        table.dataSouce = self
+        //Table Viewタップ時のdelegate先を指定
+        table.delegate = self
         
-    }
+        
+        //記事情報の取得先（最初は別で用意した配列から取得する形を目指す）
+        var titleLabel = article.title //titleLabelに記事1のタイトルを代入
+        print("\(titleLabel)")
+
+        
+
+        
+        //Qiitaサーバーに対してHTTP通信のリクエストを出してデータを取得
+        //let requestUrl = "https://qiita.com/api/v2/items"
+        
+        
+        //新着記事一覧データを格納する配列
+        //var shinchakuDataArray = NSArray()
+        
+        
+        
+        //新着記事データをテーブルビューに表示
+        }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
