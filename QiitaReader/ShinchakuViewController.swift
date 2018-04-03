ba9e8e3cbf7408981840edd342b26de201d659bc
Takuya Matsuda <takuya108817@gmail.com>
2018-04-03 16:03:31 +0900

    とりあえずShinchakuViewで記事検索ができるようにいじってみたが、起動時に記事一覧が取得できなくなったり、検索しても一番上の記事だけ表示される形にしかできない・・・

--- QiitaReader/ShinchakuViewController.swift
+++ QiitaReader/ShinchakuViewController.swift
@@ -11,9 +11,11 @@ import Alamofire
 import SwiftyJSON
 import Nuke
 
-class ShinchakuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ShinchakuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     var articles: [Article] = [] //記事を入れるプロパティarticles:構造体の配列
+    var searchResult = [Article]()
     
     //////////////////////////////////////////////////////////////////////////
     override func viewDidLoad() {
@@ -22,6 +24,13 @@ class ShinchakuViewController: UIViewController, UITableViewDelegate, UITableVie
         //記事を取得し、tableViewに記録(register)していく
         getArticles()
         self.tableView.register(UINib(nibName: "ArticleCell", bundle: nil), forCellReuseIdentifier: "ArticleCell")
+        
+        //デリゲート先を自分に設定する。
+        searchBar.delegate = self
+        //何も入力されていなくてもReturnキーを押せるようにする。
+        searchBar.enablesReturnKeyAutomatically = false
+        //検索結果配列にデータをコピーする。
+        searchResult = articles
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,38 +47,33 @@ class ShinchakuViewController: UIViewController, UITableViewDelegate, UITableVie
     /*JSON型のデータを取得し、structに変換、配列に格納するメソッド*/
     func getArticles() {
         Alamofire.request("https://qiita.com/api/v2/items").responseJSON { response in
-                guard let object = response.result.value else { //guard letで引数responseのvalueプロパティをnil剥がして、定数objectに入れる
-                    return
-                }
-                
-                let json = JSON(object) //object（1つの記事）をJSON型に変換？し、定数jsonに入れる
-                json.forEach { (_, json) in //JOSN型の定数jsonの各要素をforEachで呼び出し、それらを構造体Articleの引数とし
-                    let article = Article ( //articleを生成していく
-                        title: json["title"].string!,
-                        authorName: json["user"]["id"].string!,
-                        authorImageUrl: json["user"]["profile_image_url"].string!,
-                        goodCnt: json["likes_count"].int!,
-                        tag1: json["tags"][0]["name"].string,
-                        tag2: json["tags"][1]["name"].string,
-                        tag3: json["tags"][2]["name"].string,
-                        url: json["url"].string!
-                    )
-                    self.articles.append(article) //それを辞書の配列であるarticlesに入れていく
-                }
-                self.tableView.reloadData() //TableViewを更新
+            guard let object = response.result.value else { //guard letで引数responseのvalueプロパティをnil剥がして、定数objectに入れる
+                return
+            }
+            
+            let json = JSON(object) //object（1つの記事）をJSON型にキャスト？し、定数jsonに入れる
+            json.forEach { (_, json) in //JOSN型の定数jsonの各要素をforEachで呼び出し、それらを構造体Articleの引数とし
+                let article = Article ( //articleを生成していく
+                    title: json["title"].string!,
+                    authorName: json["user"]["id"].string!,
+                    authorImageUrl: json["user"]["profile_image_url"].string!,
+                    goodCnt: json["likes_count"].int!,
+                    tag1: json["tags"][0]["name"].string,
+                    tag2: json["tags"][1]["name"].string,
+                    tag3: json["tags"][2]["name"].string,
+                    url: json["url"].string!
+                )
+                self.articles.append(article) //それを辞書の配列であるarticlesに入れていく
+            }
+            self.tableView.reloadData() //TableViewを更新
         }
     }
     
     
-    /*TableViewに表示する記事数を返すメソッド*/
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return articles.count
-    }
-    
-    /*tableViewCellを生成し、値を設定し、そのセルを返すメソッド*/
+    /*セルを取得しデータを返すメソッド*/
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // セルを取得（生成？）する
-        let cell: ArticleCell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as! ArticleCell
+        // セルを取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as! ArticleCell
         // セルのプロパティに記事情報を設定する
         let article: Article = articles[indexPath.row]
         cell.title.text = article.title
@@ -78,11 +82,38 @@ class ShinchakuViewController: UIViewController, UITableViewDelegate, UITableVie
         cell.tag1.text = article.tag1
         cell.tag2.text = article.tag2
         cell.tag3.text = article.tag3
-        
         Manager.shared.loadImage(with: URL(string: article.authorImageUrl)!, into: cell.authorIcon)
-        
         return cell
     }
+    
+    /*TableViewに表示する記事数を返すメソッド*/
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResult.count
+    }
+    
+    //検索ボタン押下時の呼び出しメソッド
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+        
+        //検索結果配列を空にする。
+        searchResult.removeAll()
+        
+        if(searchBar.text == "") {
+            //検索文字列が空の場合はすべてを表示する。
+            searchResult = articles
+        } else {
+            //検索文字列を含むデータを検索結果配列に追加する。
+            for i in articles {
+                if i.title.contains(searchBar.text!) {
+                    searchResult.append(i)
+                }
+            }
+        }
+        
+        //テーブルを再読み込みする。
+        tableView.reloadData()
+    }
+    
     
     /*記事詳細detailViewに遷移させるメソッド*/
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
