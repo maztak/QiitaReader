e98380833d10a12dd105e7e2bd847ba2f20a8ede
Takuya Matsuda <>
2018-04-23 12:42:10 +0900

    QiitaReaderのディレクトリを変更して、変になっていた.gitや.gitignoreを綺麗に整理して、正常に戻った

--- LoginTest/Pods/Target Support Files/Pods-LoginTest/Pods-LoginTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LoginTest : NSObject
-@end
-@implementation PodsDummy_Pods_LoginTest
-@end
--- QiitaReaderProj/Pods/Target Support Files/Pods-QiitaReader/Pods-QiitaReader-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QiitaReader : NSObject
-@end
-@implementation PodsDummy_Pods_QiitaReader
-@end
