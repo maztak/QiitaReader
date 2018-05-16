28de14a72ff6cc7c3f1340937802f1a5f18daf75
Takuya Matsuda <>
2018-05-16 12:47:02 +0900

    TrendViewの未ログインアラートもview生成を外出し

--- QiitaReader/NewViewController.swift
+++ QiitaReader/NewViewController.swift
@@ -81,54 +81,6 @@ class NewViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         )
     }
     
-    // MARK: アラートビューを出すメソッド
-    func makeAlertView() {
-        // Viewを生成.
-        self.myView = UIView(frame: CGRect(x: 0, y: 0, width: 220, height: 135))
-        // myViewの背景を緑色に設定.
-        self.myView.backgroundColor = UIColor.init(red: 200, green: 200, blue: 200, alpha: 1.0)
-        // 位置を中心に設定.
-        self.myView.layer.position = CGPoint(x: (self.view.frame.width)/2, y: (self.view.frame.height)/2)
-        // 角丸
-        self.myView.layer.cornerRadius = 20.0
-        self.myView.isHidden = true
-        
-        // ボタンを生成
-        self.myButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
-        self.myButton.layer.position = CGPoint(x: (self.myView.frame.width)/2, y: (self.myView.frame.height)-50)
-        self.myButton.setTitle("リトライ", for: .normal)
-        self.myButton.setTitleColor(UIColor.blue, for: .normal)
-        self.myButton.addTarget(self, action: #selector(self.onClickMyButton), for: .touchUpInside)
-        self.myButton.isHidden = true
-        
-        //ラベルを生成
-        self.myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
-        self.myLabel.layer.position = CGPoint(x: (self.myView.frame.width)/2, y: (self.myView.frame.height)-100)
-        self.myLabel.textAlignment = NSTextAlignment.center
-        self.myLabel.text = "ネットワーク通信エラー"
-        self.myLabel.textColor = UIColor.black
-        self.myLabel.isHidden = true
-        
-        // myViewをviewに追加.
-        self.view.addSubview(self.myView)
-        // ボタンをviewに追加.
-        self.myView.addSubview(self.myButton)
-        // ラベルを追加
-        self.myView.addSubview(self.myLabel)
-    }
-    
-    /* リトライボタンイベント */
-    @objc func onClickMyButton(sender: UIButton) {
-        // リトライ処理
-        print("Retry")
-        self.myView.isHidden = true
-        self.myButton.isHidden = true
-        self.myLabel.isHidden = true
-        SVProgressHUD.show()
-        getArticles()
-    }
-    
-    
     /*データの個数を返すメソッド*/
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
@@ -200,6 +152,54 @@ class NewViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         }
         //追加した記事をコンソールに出力（確認用）
         print(realmArticle)
+    }
+    
+    
+    // MARK: アラートビューを出すメソッド
+    func makeAlertView() {
+        // Viewを生成.
+        self.myView = UIView(frame: CGRect(x: 0, y: 0, width: 220, height: 135))
+        // myViewの背景を緑色に設定.
+        self.myView.backgroundColor = UIColor.init(red: 200, green: 200, blue: 200, alpha: 1.0)
+        // 位置を中心に設定.
+        self.myView.layer.position = CGPoint(x: (self.view.frame.width)/2, y: (self.view.frame.height)/2)
+        // 角丸
+        self.myView.layer.cornerRadius = 20.0
+        self.myView.isHidden = true
+        
+        // ボタンを生成
+        self.myButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        self.myButton.layer.position = CGPoint(x: (self.myView.frame.width)/2, y: (self.myView.frame.height)-50)
+        self.myButton.setTitle("リトライ", for: .normal)
+        self.myButton.setTitleColor(UIColor.blue, for: .normal)
+        self.myButton.addTarget(self, action: #selector(self.onClickMyButton), for: .touchUpInside)
+        self.myButton.isHidden = true
+        
+        //ラベルを生成
+        self.myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+        self.myLabel.layer.position = CGPoint(x: (self.myView.frame.width)/2, y: (self.myView.frame.height)-100)
+        self.myLabel.textAlignment = NSTextAlignment.center
+        self.myLabel.text = "ネットワーク通信エラー"
+        self.myLabel.textColor = UIColor.black
+        self.myLabel.isHidden = true
+        
+        // myViewをviewに追加.
+        self.view.addSubview(self.myView)
+        // ボタンをviewに追加.
+        self.myView.addSubview(self.myButton)
+        // ラベルを追加
+        self.myView.addSubview(self.myLabel)
+    }
+    
+    /* リトライボタンイベント */
+    @objc func onClickMyButton(sender: UIButton) {
+        // リトライ処理
+        print("Retry")
+        self.myView.isHidden = true
+        self.myButton.isHidden = true
+        self.myLabel.isHidden = true
+        SVProgressHUD.show()
+        getArticles()
     }
     
     
